33fc43bd303ccf568f9b00489ac7629904f4dbe4
vedpraksh-singh <vedprak22@gmil.com>
2016-02-20 14:15:18 +0530

    Added SparkX Basic

---
+++ src/main/scala/com/tutorial/graphX/Page1.scala
@@ -0,0 +1,44 @@
+package com.tutorial.graphX
+import com.tutorial.utils.SparkCommon
+import org.apache.spark.graphx._
+
+/**
+  * Created by ved on 19/2/16.
+  */
+object Page1 {
+  val sc = SparkCommon.sparkContext
+
+  def main(args: Array[String]) {
+
+    val graph = GraphLoader.edgeListFile(sc,"src/main/resources/followers.txt")
+
+
+    /**
+      * Find the connected components
+      */
+    val cc = graph.connectedComponents().vertices
+
+    /**
+      * Join the connected components with the usernames
+      */
+    val users = sc.textFile("src/main/resources/users.txt").map { line =>
+      val fields = line.split(",")
+      (fields(0).toLong, fields(1))
+    }
+    val ccByUsername = users.leftOuterJoin(cc).map {
+      case (id, (username, cc)) => (username, cc)
+    }
+
+    /**
+      * Print the result
+      */
+    println(ccByUsername.collect().mkString("\n"))
+
+
+
+
+
+
+  }
+
+}
